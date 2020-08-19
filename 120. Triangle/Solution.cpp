fd278ef2b0b650b199428760a41b31aafdfe864e
AthulJoseph <55750689+AthulJoseph27@users.noreply.github.com>
2020-08-19 20:36:14 +0530

    Update Solution.cpp

--- 120. Triangle/Solution.cpp
+++ 120. Triangle/Solution.cpp
@@ -6,7 +6,7 @@ public:
         triangle[1][1]+=triangle[0][0];
         for(int i=2;i<triangle.size();i++){
             triangle[i][0]+=triangle[i-1][0];
-            triangle[i][triangle[i].size()-1]+=triangle[i-1][triangle[i-1].size()-1];
+            triangle[i][i]+=triangle[i-1][i-1];
             for(int j=1;j<triangle[i].size()-1;j++)
                 triangle[i][j]+=min(triangle[i-1][j-1],triangle[i-1][j]);
             }
