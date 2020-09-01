71e67afdc7276d5d5b40d39b60fb179bfecd6beb
AthulJoseph <55750689+AthulJoseph27@users.noreply.github.com>
2020-09-01 21:22:37 +0530

    Create Solution.cpp

---
+++ Largest Time for Given Digits/Solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string largestTimeFromDigits(vector<int>& A) {
+        sort(A.begin(),A.end());
+        if(A[0]>2) return "";
+        int a=0,b=0,ta=-1,tb=-1;
+        string f="",s="";
+        do{
+            a = A[1]*10+A[0];
+            b = A[2]*10+A[3];
+            if(a<24 && b < 60){
+                if(a > ta){
+                    ta = a;
+                    tb = b;
+                }else if(ta==a){
+                    tb = max(tb,b);
+                }
+            }
+        }while(next_permutation(A.begin(),A.end()));
+        
+        if(ta==-1) return "";
+        if(ta<10) f+="0"+to_string(ta);
+        else f=to_string(ta);
+        if(tb<10) s+="0"+to_string(tb);
+        else s = to_string(tb);
+        return f+":"+s;
+    }
+};
