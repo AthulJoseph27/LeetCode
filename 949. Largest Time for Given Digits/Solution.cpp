51e7e27b97b808a037e61e3d842592bafeecfcd1
AthulJoseph <55750689+AthulJoseph27@users.noreply.github.com>
2020-09-10 09:39:33 +0530

    Update Solution.cpp

--- 949. Largest Time for Given Digits/Solution.cpp
+++ 949. Largest Time for Given Digits/Solution.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
     string largestTimeFromDigits(vector<int>& A) {
+        
         sort(A.begin(),A.end());
+        
         if(A[0]>2) return "";
+        
         int a=0,b=0,ta=-1,tb=-1;
         string f="",s="";
+        
         do{
             a = A[1]*10+A[0];
             b = A[2]*10+A[3];
