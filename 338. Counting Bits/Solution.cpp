71948d8433bb2e63a44357115d94893cede132f5
AthulJoseph <55750689+AthulJoseph27@users.noreply.github.com>
2020-09-17 10:53:58 +0530

    Create Solution.cpp

---
+++ 338. Counting Bits/Solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> countBits(int num) {
+        if(num == 0) return {0};
+        if(num == 1) return {0,1};
+        vector<int>dp = {0,1,1};
+        int prev_pow = 2;
+        for(int i=3;i<=num;i++){
+            if(i==prev_pow*2){
+                prev_pow = i;
+                dp.push_back(1);
+                continue;
+            }
+            if(i%2!=0) dp.push_back(dp.back()+1);
+            else dp.push_back(i/prev_pow + dp[i%prev_pow]);
+        }
+        return dp;
+    }
+};
