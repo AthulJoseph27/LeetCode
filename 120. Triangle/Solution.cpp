561c40739a6c493e16332488fbcbe1c3c8a8d2b6
AthulJoseph <55750689+AthulJoseph27@users.noreply.github.com>
2021-01-21 11:24:34 +0530

    Update Solution.cpp

--- 120. Triangle/Solution.cpp
+++ 120. Triangle/Solution.cpp
@@ -1,16 +1,40 @@
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        if(triangle.size()==1) return triangle[0][0];
-        triangle[1][0]+=triangle[0][0];
-        triangle[1][1]+=triangle[0][0];
-        for(int i=2;i<triangle.size();i++){
-            triangle[i][0]+=triangle[i-1][0];
-            triangle[i][i]+=triangle[i-1][i-1];
-            for(int j=1;j<triangle[i].size()-1;j++)
-                triangle[i][j]+=min(triangle[i-1][j-1],triangle[i-1][j]);
-            }
         
-        return *min_element(triangle[triangle.size()-1].begin(),triangle[triangle.size()-1].end());
+        if(triangle.size() == 0)
+            return 0;
+        
+        if(triangle.size() == 1)
+            return triangle[0][0];
+        
+        vector<vector<int>> dp = {{0}};
+        for(int i=1;i<triangle.size();i++){
+            dp.push_back({});
+            for(int j=0;j<triangle[i].size();j++)
+                dp[dp.size()-1].push_back(0);
+        }
+        
+        dp[0][0] = triangle[0][0];
+        
+        for(int i=1;i<triangle.size();i++)
+            for(int j=0;j<triangle[i].size();j++){
+                
+                int a,b;
+                b = a = 1e5;
+                if(j != 0)
+                    a = dp[i-1][j-1];
+                if(j < dp[i-1].size())
+                    b = dp[i-1][j];
+                
+                dp[i][j] = min({a,b}) + triangle[i][j];
+                
+            }
+    
+        int mn = dp[dp.size()-1][0];
+        for(int i:dp[dp.size()-1])
+            mn = min(mn,i);
+        
+        return mn;
     }
 };
